9b1ae7aca917fecb606fc9ce8022bde54b3df847
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-28 04:14:23 +0200

    [ui_agent] implement dynamic submenu

--- Wrecept.Desktop/ViewModels/StageViewModel.cs
+++ Wrecept.Desktop/ViewModels/StageViewModel.cs
@@ -1,5 +1,7 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Wrecept.Desktop;
+using Wrecept.Desktop.Models;
 
 namespace Wrecept.Desktop.ViewModels;
 
@@ -15,7 +17,10 @@ public partial class StageViewModel : ObservableObject
     private int selectedIndex;
 
     [ObservableProperty]
-    private int selectedSubIndex;
+    private int selectedSubmenuIndex;
+
+    [ObservableProperty]
+    private ObservableCollection<SubmenuItem> currentSubmenuItems = new();
 
     [ObservableProperty]
     private bool isSubMenuOpen;
@@ -35,8 +40,75 @@ public partial class StageViewModel : ObservableObject
     [ObservableProperty]
     private bool showPaymentMethod;
 
+    private readonly Dictionary<int, List<SubmenuItem>> _submenuMap;
+
     public StageViewModel()
     {
+        _submenuMap = new()
+        {
+            [0] = new()
+            {
+                new SubmenuItem(0, "Bejövő szállítólevelek", () => { }),
+                new SubmenuItem(1, "Bejövő számlák aktualizálása", () => { })
+            },
+            [1] = new()
+            {
+                new SubmenuItem(0, "Termékek", () => { }),
+                new SubmenuItem(1, "Termékcsoportok", () => ShowProductGroup = true),
+                new SubmenuItem(2, "Szállítók", () => { }),
+                new SubmenuItem(3, "ÁFA-kulcsok", () => ShowTaxRate = true),
+                new SubmenuItem(4, "Fizetési módok", () => ShowPaymentMethod = true)
+            },
+            [2] = new()
+            {
+                new SubmenuItem(0, "Terméklista", () => { }),
+                new SubmenuItem(1, "Szállítók listája", () => { }),
+                new SubmenuItem(2, "Számlák listája", () => { }),
+                new SubmenuItem(3, "Készletkarton", () => { })
+            },
+            [3] = new()
+            {
+                new SubmenuItem(0, "Állományok ellenőrzése", () => { }),
+                new SubmenuItem(1, "Áramszünet után", () => { }),
+                new SubmenuItem(2, "Képernyő beállítása", () => { }),
+                new SubmenuItem(3, "Nyomtató beállítás", () => { })
+            },
+            [4] = new()
+            {
+                new SubmenuItem(0, "A program felhasználójának adatai", () => { })
+            },
+            [5] = new()
+            {
+                new SubmenuItem(0, "Kilépés", () => { })
+            }
+        };
+        LoadSubmenu(0);
+    }
+
+    private void LoadSubmenu(int mainIndex)
+    {
+        CurrentSubmenuItems.Clear();
+        if (_submenuMap.TryGetValue(mainIndex, out var list))
+        {
+            foreach (var item in list)
+                CurrentSubmenuItems.Add(item);
+        }
+        SelectedSubmenuIndex = 0;
+    }
+
+    partial void OnSelectedIndexChanged(int value)
+    {
+        LoadSubmenu(value);
+    }
+
+    public void ExecuteCurrentSubmenu()
+    {
+        HideAll();
+        if (_submenuMap.TryGetValue(SelectedIndex, out var list) &&
+            SelectedSubmenuIndex >= 0 && SelectedSubmenuIndex < list.Count)
+        {
+            list[SelectedSubmenuIndex].Action.Invoke();
+        }
     }
 
     public void HideAll()
