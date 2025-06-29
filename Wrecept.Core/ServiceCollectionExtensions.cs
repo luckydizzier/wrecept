94f4310e30b1f2a5080bafeac860eea6d26c7783
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-30 00:26:40 +0200

    [code_agent] Add WPF shell with DI

---
+++ Wrecept.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using Wrecept.Core.Services;
+
+namespace Wrecept.Core;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<IInvoiceService, InvoiceService>();
+        return services;
+    }
+}
