5922172e2e61d5efca2c84c2d3730daf30257648
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-29 17:03:37 +0200

    [ui_agent] align StageView with shared ViewModel

--- Wrecept.Desktop/Views/StageView.xaml.cs
+++ Wrecept.Desktop/Views/StageView.xaml.cs
@@ -1,22 +1,20 @@
 using System.Windows.Controls;
 using Wrecept.Desktop.ViewModels;
-using Wrecept.Desktop;
 
 namespace Wrecept.Desktop.Views;
 
 public partial class StageView : UserControl
 {
-    public StageViewModel ViewModel { get; }
+    public StageViewModel ViewModel
+    {
+        get => (StageViewModel)DataContext!;
+        set => DataContext = value;
+    }
 
 
     public StageView()
     {
         InitializeComponent();
-        ViewModel = new StageViewModel(
-            ServiceLocator.InvoiceService,
-            ServiceLocator.ProductService,
-            ServiceLocator.SupplierRepository);
-        DataContext = ViewModel;
     }
 
 }
