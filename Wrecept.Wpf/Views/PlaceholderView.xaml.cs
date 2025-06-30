06648d0ef4a11b4e2b8e18ae59f509bc904fad16
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-30 23:46:23 +0200

    [code_agent] fix invoice menu navigation and placeholder ctor

--- Wrecept.Wpf/Views/PlaceholderView.xaml.cs
+++ Wrecept.Wpf/Views/PlaceholderView.xaml.cs
@@ -1,10 +1,15 @@
 using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
 using Wrecept.Wpf.ViewModels;
 
 namespace Wrecept.Wpf.Views;
 
 public partial class PlaceholderView : UserControl
 {
+    public PlaceholderView() : this(App.Provider.GetRequiredService<PlaceholderViewModel>())
+    {
+    }
+
     public PlaceholderView(PlaceholderViewModel viewModel)
     {
         InitializeComponent();
