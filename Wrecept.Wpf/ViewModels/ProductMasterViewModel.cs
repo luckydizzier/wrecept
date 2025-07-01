39134c4fb9477caff22ef2933b0ad7cc84594d49
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-02 00:02:24 +0200

    ui_agent: keyboard-centric ProductMaster redesign

--- Wrecept.Wpf/ViewModels/ProductMasterViewModel.cs
+++ Wrecept.Wpf/ViewModels/ProductMasterViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Wrecept.Core.Models;
 using Wrecept.Core.Services;
 using System.Threading.Tasks;
@@ -12,9 +13,30 @@ public partial class ProductMasterViewModel : ObservableObject
 
     private readonly IProductService _service;
 
+    [ObservableProperty]
+    private Product? selectedProduct;
+
+    [ObservableProperty]
+    private bool isEditing;
+
+    public IRelayCommand EditSelectedCommand { get; }
+    public IRelayCommand DeleteSelectedCommand { get; }
+    public IRelayCommand CloseDetailsCommand { get; }
+
     public ProductMasterViewModel(IProductService service)
     {
         _service = service;
+        EditSelectedCommand = new RelayCommand(() => IsEditing = !IsEditing, () => SelectedProduct != null);
+        DeleteSelectedCommand = new RelayCommand(async () =>
+        {
+            if (SelectedProduct != null)
+            {
+                SelectedProduct.IsArchived = true;
+                await _service.UpdateAsync(SelectedProduct);
+                await LoadAsync();
+            }
+        }, () => SelectedProduct != null);
+        CloseDetailsCommand = new RelayCommand(() => IsEditing = false);
     }
 
     public async Task LoadAsync()
