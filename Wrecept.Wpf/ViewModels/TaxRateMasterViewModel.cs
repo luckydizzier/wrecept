73db2a1269c8f2156e09adbc33b7a385af30188a
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-03 03:03:02 +0200

    Remove redundant VAT key editor

--- Wrecept.Wpf/ViewModels/TaxRateMasterViewModel.cs
+++ Wrecept.Wpf/ViewModels/TaxRateMasterViewModel.cs
@@ -4,8 +4,6 @@ using Wrecept.Core.Services;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.Input;
-using Wrecept.Wpf.Services;
 
 namespace Wrecept.Wpf.ViewModels;
 
@@ -15,12 +13,9 @@ public partial class TaxRateMasterViewModel : EditableMasterDataViewModel<TaxRat
 
     private readonly ITaxRateService _service;
 
-    public new IRelayCommand EditSelectedCommand { get; }
-
     public TaxRateMasterViewModel(ITaxRateService service)
     {
         _service = service;
-        EditSelectedCommand = new RelayCommand(EditSelected, () => SelectedItem != null);
     }
 
     protected override Task<List<TaxRate>> GetItemsAsync()
@@ -35,26 +30,4 @@ public partial class TaxRateMasterViewModel : EditableMasterDataViewModel<TaxRat
         }
     }
 
-    private async void EditSelected()
-    {
-        if (SelectedItem is null)
-            return;
-
-        var vm = new VatKeyEditorViewModel
-        {
-            Name = SelectedItem.Name,
-            Percentage = SelectedItem.Percentage
-        };
-
-        vm.OnOk = async m =>
-        {
-            SelectedItem.Name = m.Name;
-            SelectedItem.Percentage = m.Percentage;
-            await _service.UpdateAsync(SelectedItem);
-            await LoadAsync();
-        };
-
-        DialogService.EditEntity<Views.EditDialogs.VatKeyEditorView, VatKeyEditorViewModel>(
-            vm, vm.OkCommand, vm.CancelCommand);
-    }
 }
