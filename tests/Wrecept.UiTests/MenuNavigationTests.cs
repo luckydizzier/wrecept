dfbd141b0065a7f77868759ce7a9a0a91c53a30c
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-04 02:25:28 +0200

    test: expand startup window ui tests

---
+++ tests/Wrecept.UiTests/MenuNavigationTests.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Wrecept.UiTests;
+
+[TestClass]
+public class MenuNavigationTests
+{
+    private static WindowsDriver<WindowsElement> LaunchApp()
+    {
+        var exePath = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "Wrecept.Wpf", "bin", "Debug", "net8.0-windows", "Wrecept.Wpf.exe"));
+        var options = new AppiumOptions();
+        options.AddAdditionalCapability("app", exePath);
+        return new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+    }
+
+    [TestMethod]
+    public void AboutMenu_DisplaysAboutView()
+    {
+        using var driver = LaunchApp();
+
+        driver.FindElementByName("Névjegy").Click();
+        driver.FindElementByName("A program felhasználójának adatai").Click();
+
+        Assert.IsTrue(driver.PageSource.Contains("Program neve"));
+        driver.Close();
+    }
+
+    [TestMethod]
+    public void ExitMenu_ClosesApplication()
+    {
+        using var driver = LaunchApp();
+
+        driver.FindElementByName("Vége").Click();
+        driver.FindElementByName("Kilépés").Click();
+        Thread.Sleep(1000);
+
+        Assert.ThrowsException<WebDriverException>(() => _ = driver.Title);
+    }
+}
