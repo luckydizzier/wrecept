89cc6b923a9bdf381928222e06c2a5c47fbe8596
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-06 15:18:28 +0200

    logic: trigger new invoice on up at top

--- tests/Wrecept.UiTests/InvoiceEditorTests.cs
+++ tests/Wrecept.UiTests/InvoiceEditorTests.cs
@@ -28,4 +28,42 @@ public class InvoiceEditorTests
 
         driver.Close();
     }
+
+    [TestMethod]
+    public void InvoiceList_UpOnFirstItem_OpensPrompt()
+    {
+        using var driver = LaunchApp();
+
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        var options = driver.FindElementsByName("Mintaszámok");
+        if (options.Count > 0)
+            options[0].FindElementByName("Mégse").Click();
+
+        var list = wait.Until(d => ((WindowsDriver<WindowsElement>)d)
+            .FindElementByAccessibilityId("InvoiceList"));
+        list.Click();
+
+        new OpenQA.Selenium.Interactions.Actions(driver)
+            .SendKeys(OpenQA.Selenium.Keys.Insert)
+            .Perform();
+
+        var box = wait.Until(d => ((WindowsDriver<WindowsElement>)d)
+            .FindElementByAccessibilityId("NumberBox"));
+        new OpenQA.Selenium.Interactions.Actions(driver)
+            .SendKeys(OpenQA.Selenium.Keys.Enter)
+            .Perform();
+        wait.Until(d => d.FindElementsByAccessibilityId("NumberBox").Count == 0);
+
+        list.Click();
+        new OpenQA.Selenium.Interactions.Actions(driver)
+            .SendKeys(OpenQA.Selenium.Keys.ArrowUp)
+            .Perform();
+
+        box = wait.Until(d => ((WindowsDriver<WindowsElement>)d)
+            .FindElementByAccessibilityId("NumberBox"));
+        Assert.IsNotNull(box);
+
+        driver.Close();
+    }
 }
