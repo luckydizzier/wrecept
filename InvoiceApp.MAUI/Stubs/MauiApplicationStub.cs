84e6e7316b011d1bd7751b8fb8fa65ad4faad655
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-21 20:27:10 +0200

    fix: compile Program with MauiApplication

---
+++ InvoiceApp.MAUI/Stubs/MauiApplicationStub.cs
@@ -0,0 +1,10 @@
+namespace Microsoft.Maui;
+
+public abstract class MauiApplication
+{
+    protected MauiApplication() { }
+
+    protected abstract MauiApp CreateMauiApp();
+
+    public void Run(string[] args) { }
+}
